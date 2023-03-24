92a9e54902784397f8dda2ae8f4639fcc340d542
LucasLimaPE <robertolucas011@gmail.com>
2023-03-24 18:34:18 -0300

    feat: implementa a função generatePhoneNumber .

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,10 +17,45 @@ function techList(array, name) {
   });
 }
 
+const generatePhoneNumberUtilities = {
+  makeNumber(array) {
+    const numberTogether = array.join('');
+    const prefix = numberTogether.slice(0, 2);
+    const mainNumber = numberTogether.slice(2, 11);
+
+    const result = `(${prefix}) ${mainNumber.slice(0, 5)}-${mainNumber.slice(5, 10)}`;
+
+    return result;
+  },
+
+  countRepetitions(array) {
+    const countObj = {};
+
+    for (let element of array) {
+      if (element in countObj) {
+        countObj[element] += 1;
+      } else {
+        countObj[element] = 1;
+      }
+    }
+
+    return Object.values(countObj);
+  },
+};
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (array.some((item) => item < 0 || item > 9)
+  || generatePhoneNumberUtilities.countRepetitions(array).some((item) => item >= 3)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return generatePhoneNumberUtilities.makeNumber(array);
 }
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2]));
 
 // Desafio 12
 function triangleCheck() {
